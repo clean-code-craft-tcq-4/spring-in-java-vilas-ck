25ff2c31cb946b3b0c4c32fc9488801f25a99ae3
CLK1KOR <ck.vilas@bosch.com>
2022-05-09 17:43:49 +0530

    vilas completed

--- src/test/java/statisticker/StatisticsTest.java
+++ src/test/java/statisticker/StatisticsTest.java
@@ -1,8 +1,6 @@
 package statisticker;
 
 import static org.junit.Assert.*;
-import jdk.nashorn.internal.AssertsEnabled;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
@@ -15,7 +13,7 @@ public class StatisticsTest
     public void reportsAverageMinMaxx()
     {
         Float[] numbers = {1.5f, 8.9f, 3.2f, 4.5f};
-        List<___> numberList = Arrays.asList(numbers);
+        List<Float> numberList = Arrays.asList(numbers);
 
         Statistics.Stats s = Statistics.getStatistics(numberList);
 
@@ -27,10 +25,12 @@ public class StatisticsTest
     @Test
     public void reportsNaNForEmptyInput()
     {
-        List<___> emptyList = new ArrayList<___>();
-
+        List<Float> emptyList = new ArrayList<Float>();
         Statistics.Stats s = Statistics.getStatistics(emptyList);
-
+        float epsilon = 0.001f;
+        assertEquals(s.average, Float.NaN, epsilon);
+        assertEquals(s.min, Float.NaN, epsilon);
+        assertEquals(s.max, Float.NaN, epsilon);
         //All fields of computedStats (average, max, min) must be
         //Float.NaN (not-a-number), as described in
         //https://www.geeksforgeeks.org/nan-not-number-java/
@@ -46,8 +46,8 @@ public class StatisticsTest
         StatsChecker checker = new StatsChecker(maxThreshold, alerters);
 
         Float[] numbers = {11.5f, 6.9f, 7.5f, 6.6f};
-        List<___> numberList = Arrays.asList(numbers);
-        checker.checkAndAlert(numbers);
+        List<Float> numberList = Arrays.asList(numbers);
+        checker.checkAndAlert(numberList);
         
         assertTrue(emailAlerter.emailSent);
         assertTrue(ledAlerter.ledGlows);
